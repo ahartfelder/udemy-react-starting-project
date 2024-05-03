6a264caf74daf8db5c70e3a69af282b06874fca9
ahartfelder <ahartfelder@gmail.com>
2024-05-03 11:12:49 -0300

    Create Modal for NewPost

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,26 +1,36 @@
 import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
+import Modal from "./Modal"
 import styles from './PostsList.module.css';
 
 function PostsList() {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
   const [enteredBody, setEnteredBody] = useState('');
   const [enteredAuthor, setEnteredAuthor] = useState('');
 
-  function changeBodyHandler(event) {
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
+
+  function bodyChangeHandler(event) {
     setEnteredBody(event.target.value);
   }
 
-  function changeAuthorHandler(event) {
+  function authorChangeHandler(event) {
     setEnteredAuthor(event.target.value);
   }
 
   return (
     <>
-      <NewPost 
-        onBodyChange={changeBodyHandler}
-        onAuthorChange={changeAuthorHandler}
-      />
+      {modalIsVisible &&
+        <Modal onClose={hideModalHandler}>
+          <NewPost 
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      }
       <ul className={styles.posts}>
         <Post key={1} author={enteredAuthor} body={enteredBody} />
         <Post key={2} author="Carlos" body="Hey Ya" />
