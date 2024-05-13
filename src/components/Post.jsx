84127cf48a2d3e2522cb4600bbf427821b72f2ee
ahartfelder <ahartfelder@gmail.com>
2024-05-13 15:40:15 -0300

    create new post + view post with routing

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,14 @@
+import { Link } from 'react-router-dom';
 import styles from './Post.module.css';
 
 function Post({post}) {
   const { id, author, body } = post;
   return (
-    <li className={styles.post} key={id}>
-      <p className={styles.author}>{author}</p>
-      <p className={styles.text}>{body}</p>
+    <li className={styles.post}>
+      <Link to={id}>
+        <p className={styles.author}>{author}</p>
+        <p className={styles.text}>{body}</p>
+      </Link>
     </li>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import './index.css'
-import Posts from './router/Posts'
-import NewPost from './router/NewPost';
+import './index.css';
 import RootLayout from './router/RootLayout';
+import Posts, { loader as postsLoader } from './router/Posts';
+import NewPost, { action as newPostAction } from './router/NewPost';
+import PostDetails, { loader as postLoader } from './router/PostDetails';
 
 const router = createBrowserRouter([
-  { path: '/', element: <RootLayout />, children: [
-    { path: '/', element: <Posts />, children: [
-      { path: '/create-post', element: <NewPost /> }
-    ] },
-  ] }
+  { 
+    path: '/', 
+    element: <RootLayout />, 
+    children: [
+      { 
+        path: '/', 
+        element: <Posts />, 
+        loader: postsLoader,
+        children: [
+          { 
+            path: '/create-post', 
+            element: <NewPost />, 
+            action: newPostAction
+          },
+          {
+            path: '/:id',
+            element: <PostDetails />,
+            loader: postLoader
+          }
+        ]
+      },
+    ]
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
